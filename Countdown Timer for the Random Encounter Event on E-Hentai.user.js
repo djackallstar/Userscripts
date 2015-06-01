55654312f0338acaec6c50eedb39a8f8ff4d7323
djackallstar <djackallstar@users.noreply.github.com>
2015-06-01 11:42:15 +0800

    Update Countdown Timer for the Random Encounter Event on E-Hentai.user.js

--- Countdown Timer for the Random Encounter Event on E-Hentai.user.js
+++ Countdown Timer for the Random Encounter Event on E-Hentai.user.js
@@ -102,7 +102,11 @@ if(/(\.e-hentai\.org\/)|(^e-hentai.org\/)/.test(loc.hostname+'/') && !/\/palette
             var ss = Math.floor(diff % 60) + ''
             ss = (ss.length >= 2 ? ss : '0' + ss)
             timer_box.textContent = mm + ':' + ss + ', re_cnt=' + get_cookie('re_cnt')
-            if((mm == '00') && /50|40|30|20|10|05/.test(ss)) { alert('Random Encounter') }
+            try {
+                if(doc.getElementById('eventpane').getElementsByTagName('div')[1].getElementsByTagName('a')[0].textContent != 'HentaiVerse') {
+                    if((mm == '00') && /50|40|30|20|10|05/.test(ss)) { alert('Random Encounter') }
+                }
+            } catch(e) {}
         }
         setTimeout(update_timer, 1000)
     }
