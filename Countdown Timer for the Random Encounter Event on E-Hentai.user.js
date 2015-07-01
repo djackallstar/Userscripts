f2f55be38ed9f63c1bd18edf3d81fa87cea639d3
djackallstar <djackallstar@users.noreply.github.com>
2015-07-01 11:17:36 +0800

    Update Countdown Timer for the Random Encounter Event on E-Hentai.user.js

--- Countdown Timer for the Random Encounter Event on E-Hentai.user.js
+++ Countdown Timer for the Random Encounter Event on E-Hentai.user.js
@@ -110,7 +110,7 @@ if(!document.querySelector('*[name="ipb_login_submit"]') && /(\.e-hentai\.org\/)
                 try {
                     if(doc.getElementById('eventpane').getElementsByTagName('div')[1].getElementsByTagName('a')[0].textContent != 'HentaiVerse') {
                         if(!document.getElementById('re_snd')) {
-                            var audio = new Audio('http://www.freesound.org/data/previews/238/238995_71257-lq.mp3')
+                            var audio = new Audio('http://www.freesound.org/data/previews/234/234524_4019029-lq.mp3')
                             audio.id = 're_snd'
                             audio.volume = 0.1
                             audio.loop = true
