df6319aec84f742de8c7afe8e998473f81293be3
djackallstar <djackallstar@users.noreply.github.com>
2015-08-16 23:06:39 +0800

    Update RealtimeAuctionCountdownTimer.user.js

--- RealtimeAuctionCountdownTimer.user.js
+++ RealtimeAuctionCountdownTimer.user.js
@@ -50,7 +50,8 @@ var init_timer = function(timer, thread_url) {
 var threads = doc.querySelectorAll('table.ipbtable>tbody>tr>td.row1>div:last-child')
 for(var i=threads.length-1; i>=0; i--) {
     var title = threads[i].textContent
-    if(/\[(auction|lottery|free)\b/i.test(title)) {
+    //if(/\[(auction|lottery|free)\b/i.test(title)) {
+    if(/\[auction\b/i.test(title)) {
         (function() {
             var thread_url = threads[i].querySelector('span>a:first-child').href
             var timer = doc.createElement('IFRAME')
