39a771831196f40662e47e9dc2bf655fa7da797f
djackallstar <djackallstar@users.noreply.github.com>
2015-12-20 16:51:02 +0800

    Update Dmzj Anime Downloader.user.js

--- Dmzj Anime Downloader.user.js
+++ Dmzj Anime Downloader.user.js
@@ -203,7 +203,7 @@ if(/^http:\/\/donghua\.dmzj\.com\//.test(href)) {
                             parse_url = 'http://www.flvxz.com/?url=' + encodeURIComponent(vid_url)
                         }
                         else if(parse_service == 'flvcd') {
-                            parse_url = 'http://www.flvcd.com/parse.php?kw=' + encodeURIComponent(vid_url)
+                            parse_url = 'http://www1.flvcd.com/parse.php?kw=' + encodeURIComponent(vid_url)
                         }
                         else if(parse_service == 'none') {
                             parse_url = vid_url
